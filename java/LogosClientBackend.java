c809f486fd5e914d2d035d7986574b0be5901665
hyperrixel <50220578+hyperrixel@users.noreply.github.com>
2021-10-03 23:16:40 +0200

    Add files via upload

---
+++ java/LogosClientBackend.java
@@ -0,0 +1,12 @@
+package com.hyperrixel.hdls;
+
+import java.util.PriorityQueue;
+
+class LogosClientBackend {
+
+  private boolean _isHealthy;
+  private PriorityQueue<String>;
+
+  LogosClientBackend();
+
+}
